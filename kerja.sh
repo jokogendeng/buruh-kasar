6dedbc9ddc1721af53966719244e8b795c96fb70
oyi77 <mbahkoe.pendekar@gmail.com>
2021-08-13 05:28:52 +0700

    update:

--- kerja.sh
+++ kerja.sh
@@ -3,12 +3,10 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
-git clone https://jokoGendeng@bitbucket.org/jokoGendeng/gendenganyaran.git
-cd gendenganyaran
-rm setup.json
-mv setup2.json setup.json
-chmod +x asucok.sh
-bash asucok.sh
+git clone https://jokoGendeng@bitbucket.org/jokoGendeng/dungdung.git
+cd dungdung
+chmod +x gas
+./gas keun.ini
 
      echo COUNTER $COUNTER
      let COUNTER-=1
